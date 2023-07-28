3073f64dde873ec13e5ecbe553c6b424b8c7c699
Aditya MP <211wadityaw112@gmail.com>
2023-07-28 20:13:53 +0700

    api called successfully

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -14,6 +14,7 @@ export async function POST(req: NextRequest){
         const response = await createResponse(isiPesan.toString());
         return NextResponse.json({message: 'success', payload: response}, {status: 200})
     } catch (error) {
+        console.log(error)
         return NextResponse.json({message: 'error', payload: error}, {status: 500})   
     }
 }
--- lib/data/openai.ts
+++ lib/data/openai.ts
@@ -2,32 +2,30 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  apiBase: process.env.OPENAI_API_BASE
+  basePath: process.env.OPENAI_API_BASE
 });
 const openai = new OpenAIApi(configuration);
 
 function scaffold(pesan: string) {
     return {
-        "command":"From 0-100, rate this message for possibilty of being a spam email with 100 is spam, and 0 is safe. Send your response with the provided schema and in selected language",
+        "command":"From 0-100, rate this message for possibilty of being a spam email with 100 is spam, and 0 is safe. Send your response with the provided schema",
         "message":pesan,
-        "ai_lang_setting":"id",
-        "ai_response_schema":{
+        "response_schema":{
             "probability":"int",
-            "reasosing":"str",
+            "reasoning":"str",
         }
     }
 }
 
 export async function createResponse(isiPesan: string) {
+    console.log((new Date()).toISOString()+" - called openai api")
     const pesan = JSON.stringify(scaffold(isiPesan));
     const chatCompletion = await openai.createChatCompletion({
-        model: "llama-2-7b-chat",
+        model: "gpt-3.5-turbo",
         messages: [{role: "user", content: pesan}],
-      });
-    console.log(chatCompletion.data.choices[0].message);
-    const response = JSON.parse(chatCompletion.data.choices[0].message);
-    console.log(response);
-    response.metadata = chatCompletion;
-    return response
+    });
 
+    const response = JSON.parse(chatCompletion.data.choices[0].message.content);
+
+    return response
 }
