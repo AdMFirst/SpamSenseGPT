f90cb11e06de608d8eeda94bf7da0c19d116dcb9
Aditya MP <211wadityaw112@gmail.com>
2023-07-31 11:31:16 +0700

    main function done

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Profile() {
 
     const displayGPTResult = () => {
         if (hasilGPT !== null){
-            var borderClass = 'flex-shrink-1 p-4 border-4 rounded-lg ';
+            var borderClass = 'border-4 rounded-lg ';
             var pathColor = 'rgb(107 114 128)'
             if (hasilGPT.probability >= 0 && hasilGPT.probability < 30){
                 borderClass = borderClass + "border-green-500";
@@ -77,13 +77,13 @@ export default function Profile() {
             } else {
                 borderClass = borderClass + "border-gray-500";
             }
-            return <div className='flex p-4'>
-                <div className={borderClass}>
+            return <div className='flex py-2 px-4 max-w-3xl md:flex-row flex-col-reverse justify-center items-center'>
+                <div className={"basis-3/4 p-4 "+ borderClass}>
                     <p>
                         {hasilGPT.reasoning}
                     </p>
                 </div>
-                <div className='flex-shrink-1 p-4'>
+                <div className='basis-1/4 p-4 xl:max-w-none' style={{maxWidth:"50%"}}>
                     <CircularProgressbar 
                         value={hasilGPT.probability} 
                         text={`${hasilGPT.probability}%`} 
@@ -160,42 +160,58 @@ export default function Profile() {
  
     if (isLoading) return <div className="h-screen">
         <div className="flex justify-center items-center h-full">
-            <ScaleLoader color="#36d7b7" />
+            <ScaleLoader color="#5FCFFF"  />
         </div>
     </div>
     
     if (!data) return <p>No profile data</p>
     
     return (
-        <div className='flex flex-col justify-center items-center h-full'>
-            <h1>Welcome!</h1>
-            <h3>Token tersisa: {data.token}</h3>
-            <form onSubmit={handleSubmit}>
-                <textarea
-                    value={isiBoxPesan}
-                    onChange={handleChangeBoxPesan}
-                    maxLength={500}
-                    rows={5}
-                    cols={40}
-                    name='pesan'
-                    id="pesan"
-                /><br/>
-                <input type="hidden" name="uuid" id="uuid" value={data.uuid} />
-                <button type='submit' className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Submit
-                </button>
-                <button type="button" onClick={handleClickPasteButton} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Paste
-                </button>
-                <button type="button" onClick={()=> {setIsiBoxPesan('')}} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Clear
-                </button>
-            </form>
-            <button className="btn btn-blue" onClick={()=>{setPesanLoading(!isPesanLoading)}} >reset loading</button>
-            <BarLoader color="#36d7b7" loading={isPesanLoading} cssOverride={{width:'80%'}} />
-            
+        <div className='flex flex-col justify-center items-center'>
+            <div id="title" className='pb-2 pt-6 px-4'>
+                <h1>Deteksi Pesan Spam Dengan Teknologi AI</h1>
+            </div>
+            <div id="frame4-form" className='py-2 px-4'>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <textarea
+                            value={isiBoxPesan}
+                            onChange={handleChangeBoxPesan}
+                            maxLength={500}
+                            rows={10}
+                            cols={100}
+                            name='pesan'
+                            id="pesan"
+                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-400 focus:border-blue-500"
+                            placeholder='Ketik atau tempel pesan anda disini untuk mulai mendeteksi pesan anda'
+                            required
+                        />
+                        <input type="hidden" name="uuid" id="uuid" value={data.uuid} />
+                    </div>
+                    <div className='pt-4 flex flex-row justify-center items-center'>
+                        <button type="button" onClick={()=> {setIsiBoxPesan('')}} className="mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            Clear
+                        </button>
+                        <button type="button" onClick={handleClickPasteButton} className="mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            Paste
+                        </button>
+                        <button type='submit' className="mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            Submit <br/>
+                        </button>
+                    </div>
+                    <div className='pt-1 flex flex-row justify-center items-center'>
+                        <p className='text-gray-400'>Token tersisa: {data.token} </p>
+                    </div>
+                </form>
+            </div>
+            <div id="loading bar" className='w-[75%]'>
+                <BarLoader color="#5FCFFF" loading={isPesanLoading} cssOverride={{width:'100%'}} />
+            </div>
             {displayGPTResult()}
-            <p>your user id: {data.uuid}</p>
+            <div id='Footer'>
+                <p>your user id: {data.uuid}</p>
+            </div>
+            
         </div>
     )
 }
