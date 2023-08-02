82c4a017bfced01bc9db6569e6211251bf32a3a3
Aditya MP <211wadityaw112@gmail.com>
2023-08-02 14:34:39 +0700

    fix  react/no-unescaped-entities

--- app/blog/privacypolicy/page.tsx
+++ app/blog/privacypolicy/page.tsx
@@ -51,11 +51,11 @@ export default function TermsOfUse() {
 
                         <h2 className="mt-2">Log Files</h2>
 
-                        <p className="mt-2">SpamSenseGPT follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Our Privacy Policy was created with the help of the <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.privacypolicyonline.com/privacy-policy-generator/">Privacy Policy Generator</a>.</p>
+                        <p className="mt-2">SpamSenseGPT follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of {"hosting services' analytics."} The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, {"tracking users' movement on the website"}, and gathering demographic information. Our Privacy Policy was created with the help of the <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.privacypolicyonline.com/privacy-policy-generator/">Privacy Policy Generator</a>.</p>
 
                         <h2 className="mt-2">Cookies and Web Beacons</h2>
 
-                        <p className="mt-2">Like any other website, SpamSenseGPT uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+                        <p className="mt-2">{"Like any other website, SpamSenseGPT uses \"cookies\". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information."}</p>
 
                         <p className="mt-2">For more general information on cookies, please read <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.privacypolicyonline.com/what-are-cookies/">the "Cookies" article from the Privacy Policy Generator</a>.</p>
 
@@ -63,17 +63,17 @@ export default function TermsOfUse() {
 
                         <p className="mt-2">You may consult this list to find the Privacy Policy for each of the advertising partners of SpamSenseGPT.</p>
 
-                        <p className="mt-2">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on SpamSenseGPT, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+                        <p className="mt-2">{"Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on SpamSenseGPT, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit."}</p>
 
                         <p className="mt-2">Note that SpamSenseGPT has no access to or control over these cookies that are used by third-party advertisers.</p>
 
                         <h2 className="mt-2">Third Party Privacy Policies</h2>
 
-                        <p className="mt-2">SpamSenseGPT's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+                        <p className="mt-2">{"SpamSenseGPT's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options."} </p>
 
-                        <p className="mt-2">You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
+                        <p className="mt-2">{"You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?"}</p>
 
-                        <h2 className="mt-2">Children's Information</h2>
+                        <h2 className="mt-2">{"Children's Information"}</h2>
 
                         <p className="mt-2">Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
