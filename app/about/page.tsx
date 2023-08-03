da3cb282926c48d08f9723b86ba42ac2aacd9c7b
Aditya MP <211wadityaw112@gmail.com>
2023-08-03 10:33:39 +0700

    merge main

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,24 +4,46 @@ import '../globals.css'
 
 export default function about() {
   return (
-    <>
+    <div className='bg-gray-100 h-full pb-10'>
       
     <center>
       <h1 className='font-bold text-[25px] md:text-[39px] underline underline-offset-8'>About Us</h1>
-      <img src="icon-512x512.png" alt="logo" className="m-5 w-40 md:w-max"/>
+      <img src="icon-512x512.png" alt="logo" className="m-5 w-40"/>
     </center>
     <div className='text-base md:text-2xl px-9 md:px-28 text-justify'>
       <p className='py-3 md:py-5'>Selamat datang di era baru dalam keamanan digital! Dengan bangga kami persembahkan SpamSenseGPT, aplikasi deteksi spam berbasis AI yang canggih dan dapat diandalkan. Terinspirasi oleh semangat inovasi, kami berkomitmen untuk memberikan perlindungan tanpa batas dari ancaman spam kepada Anda dan seluruh pengguna di Indonesia.</p>
       <p className='py-3 md:py-5'>SpamSenseGPT bukan hanya sekadar aplikasi biasa. Ini adalah jawaban kami atas seruan untuk melindungi dunia digital kita dari kehadiran mengganggu spam. Dengan teknologi AI yang inovatif, kami memastikan setiap email, pesan, dan komunikasi digital Anda bebas dari pesan-pesan spam yang mengacaukan.</p>
       <p className='py-3 md:py-5'>Kemampuan deteksi otomatis kami didukung oleh sistem AI yang pintar, memungkinkan SpamSenseGPT untuk secara cerdas memilah dan menyaring spam dengan tepat sasaran. Pesan penting Anda tetap tidak terpengaruh, sementara spam akan diatasi dengan cepat dan efisien.</p>
       <p className='py-3 md:py-5'>Keamanan data pengguna adalah prioritas utama kami. Dengan menggunakan teknologi terdepan dalam perlindungan data, Anda dapat yakin bahwa informasi pribadi Anda tetap aman dan terjaga kerahasiaannya.</p>
+      
+      <h2 className='py-3 md:py-5 text-2xl font-bold dark:text-white'>Tentang <span className=' text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>SpamSenseGPT</span></h2>
+      <p className='py-3 md:py-5'>Di SpamsenseGPT, kami sangat bersemangat dalam memerangi pesan spam dan menciptakan lingkungan online yang lebih aman. Kami memanfaatkan kekuatan teknologi model bahasa GPT untuk mengembangkan sistem pendeteksi spam yang tangguh yang meningkatkan pengalaman pengguna dan melindungi individu dan organisasi dari gangguan pesan yang tidak diinginkan.  </p>
+      <p className='py-3 md:py-5'>Tim ahli kami yang berdedikasi dalam rekayasa perangkat lunak berkomitmen untuk mengembangkan solusi mutakhir yang selalu berada di depan dalam perkembangan teknik spam. Dengan memanfaatkan kemampuan model GPT, kami menggunakan algoritme canggih dan analisis cerdas untuk secara akurat mendeteksi dan menyaring pesan spam di berbagai saluran komunikasi. </p>
+      <p className='py-3 md:py-5'> Kami menghargai kepercayaan pengguna kami dan berusaha untuk memberikan pengalaman yang intuitif dan mudah digunakan. Pendekatan kami yang berpusat pada pengguna memungkinkan kami untuk memahami dan mengatasi tantangan unik yang terkait dengan deteksi spam, menyesuaikan solusi kami untuk memenuhi kebutuhan spesifik individu dan bisnis. </p>
+      <p className='py-3 md:py-5'>Bergabunglah bersama kami dalam memerangi spam dan rasakan kekuatan SpamsenseGPT. Bersama-sama, kita dapat menciptakan lingkungan digital bebas spam yang mendorong komunikasi yang produktif dan meningkatkan keamanan online untuk semua pengguna.</p>
+
+      <h2 className='py-3 md:py-5 text-2xl font-bold dark:text-white'>Siapa Kami</h2>
+      <p className='py-3 md:py-5'>Kami adalah tim pengembang yang berdedikasi dan bersemangat untuk mengembangkan solusi inovatif dalam berbagai bidang dalam kehidupan sehari-hari. Sebagai peserta magang di Perusahaan ProCodeCG, kami diberi kesempatan untuk mengerjakan proyek-proyek menantang yang berkontribusi pada kemajuan penggunaan teknologi AI dalam berbagai bidang dalam kehidupan sehari-hari.</p>
+      <p className='py-3 md:py-5'>Selain magang, kami juga merupakan mahasiswa di Telkom University, tempat kami menempuh pendidikan di bidang ilmu software engineering dan bidang terkait. Latar belakang akademis kami memberi kami dasar yang kuat dalam pemrograman, algoritme, dan pembelajaran mesin, yang memungkinkan kami untuk menerapkan pengetahuan teoretis ke dalam pemecahan masalah praktis.</p>
+      <p className='py-3 md:py-5'>Melalui pengalaman magang kami di Perusahaan ProCodeCG dan studi akademis kami di Telkom University,, kami telah memperoleh wawasan yang berharga dan paparan terhadap praktik industri terbaru dan teknologi mutakhir. Kami terus berupaya untuk memperluas pengetahuan dan keterampilan kami, mengikuti perkembangan metodologi pendeteksian spam dan memanfaatkan teknologi yang sedang berkembang, seperti model bahasa GPT.</p>
+      <p className='py-3 md:py-5'>Kami bangga dengan komitmen kami terhadap keunggulan, dengan terus mendorong batas-batas teknologi komputer. Melalui penelitian dan pengembangan yang cermat, kami selalu menjadi yang terdepan dalam kemajuan di bidang ini, memastikan bahwa solusi kami adalah yang terbaru dan mampu menangani segala masalah yang muncul.</p>
+      <p className='py-3 md:py-5'>Inti dari pekerjaan kami adalah penekanan yang kuat pada kepuasan pengguna. Kami memahami rasa frustrasi yang disebabkan oleh berbagai macam permasalah dan didorong oleh keinginan untuk menciptakan lingkungan online yang lebih aman bagi individu dan organisasi. Dengan mengembangkan sistem yang mudah digunakan dan intuitif, kami bertujuan untuk memberdayakan pengguna kami dengan alat yang efektif yang meningkatkan pengalaman online mereka dan melindungi komunikasi digital mereka.</p>
+
       <p className='text-lg md:text-3xl'>Founder:</p>
-      <div className='pl-5'>
+      <div className='pl-5 py-2'>
         <p>1. Habib Muhammad Irsyad</p>
         <p>2. Aditya Mardi Pratama</p>
         <p>3. Jahfal Mudrik Ramadhan</p>
       </div>
+
+      <div className='py-3 text-center'>
+        <h2 className='py-3 md:py-5 text-2xl font-bold dark:text-white'>Bacaan Lanjutan</h2>
+        <a href='https://procodecg.com/about.html' className='py-3 md:py-5 text-xl font-bold dark:text-white hover:underline'>About ProcodeCG</a> <br/>
+        <a href='https://telkomuniversity.ac.id/en/about' className='py-3 md:py-5 text-xl font-bold dark:text-white hover:underline'>About Telkom University</a> <br/>
+        <a href='https://openai.com/about' className='py-3 md:py-5 text-xl font-bold dark:text-white hover:underline'>About OpenAI</a>  <br/>
+        
+      </div>
     </div>
-    </>
+    </div>
   )
 }
